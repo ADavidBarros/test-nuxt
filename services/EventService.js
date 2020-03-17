d7c9392bdb85a772cc79d7a579889f8a574686e7
A. David Barros <dbpiano@gmail.com>
2020-03-16 22:57:56 -0400

    Saving changes before deploying to heroku

--- services/EventService.js
+++ services/EventService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // import NProgress from 'nprogress' // <--- Import the library
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3001`,
+  baseURL: `https://my-json-server.typicode.com/ADavidBarros/test-nuxt/`,
   withCredentials: false, // This is the default
   headers: {
     Accept: 'application/json',
@@ -24,7 +24,7 @@ const apiClient = axios.create({
 // })
 
 export default {
-  getEvents(perPage = 3, page = 1) {
+  getEvents(perPage = 6, page = 1) {
     return apiClient.get(
       '/events?_limit=' + perPage + '&_page=' + page + '&_sort=id&_order=desc'
     )
