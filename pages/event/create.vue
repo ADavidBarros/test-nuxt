0788932cc331219dd5dd644993613bd58332118f
A. David Barros <dbpiano@gmail.com>
2020-03-16 20:23:07 -0400

    Simple axios api also SEO setup

---
+++ pages/event/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Create</h1>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: 'Create and Event',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'You can Create a new event'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped></style>
